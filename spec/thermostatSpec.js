90e56c4f125c0f6fd0f7ad4b085d21a0ecf9f6d4
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-16 10:46:26 +0000

    Create power saving mode with first test

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,17 +11,21 @@ describe('Thermostat', function(){
     expect(thermostat._temperature).toEqual(20);
   });
 
-  it('return the current temperature', function() {
-    expect(thermostat.checkCurrentTemperature()).toEqual(20);
-  });
-
-  it('sets minimum temperature', function(){
+  it('has a minimum temperature', function(){
     expect(thermostat._minTemperature).toEqual(10);
   });
 
-   it('sets maximum temperature', function(){
-     expect(thermostat._maxTemperature).toEqual(32);
-   });
+  it('has a maximum temperature', function(){
+   expect(thermostat._maxTemperature).toEqual(32);
+  });
+
+  it('has power saving on to begin', function(){
+    expect(thermostat._powerSaving).toEqual(true);
+  });
+
+  it('return the current temperature', function() {
+    expect(thermostat.checkCurrentTemperature()).toEqual(20);
+  });
 
   describe('Increase', function(){
 
@@ -44,11 +48,11 @@ describe('Thermostat', function(){
       thermostat.decrease();
       expect(thermostat._temperature).toEqual(20);
     });
+
     it('wont\'t allow to decrease past min temperature', function(){
       thermostat._temperature = 10
       thermostat.decrease();
       expect(thermostat.decrease()).toEqual('Minimum temperature is 10!')
     });
-
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -4,6 +4,7 @@ function Thermostat (){
   this._temperature = 20
   this._minTemperature = 10
   this._maxTemperature = 32
+  this._powerSaving = true
 };
 
 Thermostat.prototype.checkCurrentTemperature = function () {
@@ -12,14 +13,16 @@ Thermostat.prototype.checkCurrentTemperature = function () {
 
 Thermostat.prototype.increase = function () {
   if (this._temperature <= 32) {
-  this._temperature += 1;
+    this._temperature += 1;
   } else {
-  return 'Maximum temperature is 32!';
+    return 'Maximum temperature is 32!';
   }
 };
 
 Thermostat.prototype.decrease = function() {
-  if (this._temperature >= 10) {this._temperature -= 1;}
-  else
-  {return 'Minimum temperature is 10!'}
+  if (this._temperature >= 10) {
+    this._temperature -= 1;
+  } else {
+    return 'Minimum temperature is 10!'
+  }
 };
