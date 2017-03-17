7c61588d813ac754bfd9be58d6b03a6eae043594
Ryan <Ryanchu.ws@gmail.com>
2017-03-17 15:49:24 +0000

    Added Ajax functionality

--- views/thermostat_jquery.js
+++ views/thermostat_jquery.js
@@ -28,6 +28,15 @@ $('#power-saving').click(function(){
    $('#usage').text(thermostat.checkUsage());
   //  $('#ps').text(thermostat.checkMaxTemp());
  };
+
+
+ $.get("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=c0c1146523134c21458501cd8b2ee3dc&units=metric", function(response){
+   console.log(response);
+   console.log(response.main.temp)
+   console.log(response.weather[0].description)
+ });
+
+
 });
 
 // check mode and check usage functions needed to be implemented so it updates
