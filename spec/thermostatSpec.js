49e831414d1f44f9baae98ca35028d4a4e22fb54
Ryan <Ryanchu.ws@gmail.com>
2017-03-17 11:41:01 +0000

    all tests passing, fixed tests where temperature was manipulated twice, throwing duplicate error messages

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,3 +1,5 @@
+debugger;
+
 'use strict';
 
 describe('Thermostat', function() {
@@ -43,7 +45,6 @@ describe('Thermostat', function() {
     it('wont\'t allow to increase past max temperature', function() {
       thermostat.switchMode();
       thermostat._temperature = 32;
-      thermostat.increase();
       expect(function(){thermostat.increase();}).toThrowError(maxMessage);
     });
   });
@@ -58,7 +59,6 @@ describe('Thermostat', function() {
 
     it('wont\'t allow to decrease past min temperature', function() {
       thermostat._temperature = 10;
-      thermostat.decrease();
       expect(function(){thermostat.decrease();}).toThrowError(minMessage)
     });
   });
@@ -80,7 +80,6 @@ describe('Thermostat', function() {
 
     it('wont\'t allow to increase past max temperature when ON', function() {
       thermostat._temperature = 25;
-      thermostat.increase();
       expect(function(){thermostat.increase();}).toThrowError(maxMessage);
     });
 
