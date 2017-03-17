66fb25a3ed9bdb675235dad885c3685c84773d61
Ryan <Ryanchu.ws@gmail.com>
2017-03-17 16:52:18 +0000

    select location functionality implemented via form + jquery preventdefault

--- views/thermostat_jquery.js
+++ views/thermostat_jquery.js
@@ -29,13 +29,13 @@ $('#power-saving').click(function(){
   //  $('#ps').text(thermostat.checkMaxTemp());
  };
 
-
- $.get("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=c0c1146523134c21458501cd8b2ee3dc&units=metric", function(response){
-   $('#location-temp').text(response.main.temp + "°C")
-   $('#weather-description').text(response.weather[0].description)
- });
-
+$("#location-form").on('submit', function(){
+  event.preventDefault()
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $('#value').val() + "&APPID=c0c1146523134c21458501cd8b2ee3dc&units=metric";
+  $.get(url, function(response){
+    $('#location-temp').text(response.main.temp + "°C")
+    $('#weather-description').text(response.weather[0].description)
+  })
+})
 
 });
-
-// check mode and check usage functions needed to be implemented so it updates
